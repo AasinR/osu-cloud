8cbc1e3c1fb91651fde0c61b09213f0be7e50199
AasinR <bazsi2001018@gmail.com>
2022-12-09 13:45:49 +0100

    updated SearchFilter (I dont remember anything)

--- src/components/SearchFilter.tsx
+++ src/components/SearchFilter.tsx
@@ -10,30 +10,49 @@ function SearchFilter({ devices }: { devices: Device[] }) {
   return (
     <div className="search-filter">
       <div className="search-bar">
-        <input className="search-bar-input" type="text" />
+        <input
+          className="search-bar-input"
+          type="text"
+          placeholder="Search..."
+          spellCheck={false}
+        />
       </div>
-      <div className="search-filter-sort">
-        Sort by:
-        <select className="search-filter-select">
-          {sortOptions.map((option) => (
-            <option key={option.value} value={option.value}>
-              {option.name}
-            </option>
-          ))}
-        </select>
-      </div>
-      <div className="search-filter-device">
-        Device:
-        <select className="search-filter-select">
-          <option key={1} value={1}>
-            All
-          </option>
-          {devices.map((option) => (
-            <option key={option.uuid} value={option.uuid}>
-              {option.name}
-            </option>
-          ))}
-        </select>
+      <div className="search-filter-menu">
+        <div className="search-filter-sort">
+          <label htmlFor="sort-select">
+            Sort by:
+            <select id="sort-select" className="search-filter-select">
+              {sortOptions.map((option) => (
+                <option key={option.value} value={option.value}>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </label>
+          <label htmlFor="sort-order">
+            Reverse:
+            <input
+              id="sort-order"
+              className="search-filter-checkbox"
+              type="checkbox"
+            />
+          </label>
+        </div>
+        <div className="search-filter-device">
+          <label htmlFor="device-select">
+            Device:
+            <select id="device-select" className="search-filter-select">
+              <option key={1} value={1}>
+                All
+              </option>
+              {devices.map((option) => (
+                <option key={option.uuid} value={option.uuid}>
+                  {option.name}
+                </option>
+              ))}
+            </select>
+          </label>
+        </div>
       </div>
     </div>
   );
