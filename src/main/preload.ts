b28eeb75a2c9127c7de23f851e7697d588a417fd
AasinR <bazsi2001018@gmail.com>
2023-02-01 01:54:00 +0100

    cleaner ipc

--- src/main/preload.ts
+++ src/main/preload.ts
@@ -1,17 +1,33 @@
 import { contextBridge, ipcRenderer } from 'electron';
 import { channel } from './ipc/channels';
 
-contextBridge.exposeInMainWorld('electron', {
-    openExternal: (url: string) =>
-        ipcRenderer.invoke(channel.openExternal, url),
-    showDialog: (type: 'openFile' | 'openDirectory') =>
-        ipcRenderer.invoke(channel.showDialog, type),
-    getDevice: () => ipcRenderer.invoke(channel.getDevice),
-    getSaveData: () => ipcRenderer.invoke(channel.getSaveData),
-    checkGameFolder: () => ipcRenderer.invoke(channel.checkGameFolder),
-    getSettings: () => ipcRenderer.invoke(channel.getSettings),
-    setSettings: (key: string | SettingsData, value?: unknown) =>
-        ipcRenderer.invoke(channel.setSettings, key, value),
-    selectGoogleDrive: (filePath: string) =>
-        ipcRenderer.invoke(channel.selectGoogleDrive, filePath),
-});
+const electronHandler = {
+    openExternal(url: string) {
+        ipcRenderer.invoke(channel.openExternal, url);
+    },
+    showDialog(type: 'openFile' | 'openDirectory'): Promise<string> {
+        return ipcRenderer.invoke(channel.showDialog, type);
+    },
+    getDevice(): Promise<Device> {
+        return ipcRenderer.invoke(channel.getDevice);
+    },
+    getSaveData(): Promise<SaveData> {
+        return ipcRenderer.invoke(channel.getSaveData);
+    },
+    checkGameFolder(): Promise<boolean> {
+        return ipcRenderer.invoke(channel.checkGameFolder);
+    },
+    getSettings(): Promise<SettingsData> {
+        return ipcRenderer.invoke(channel.getSettings);
+    },
+    setSettings(key: string | SettingsData, value?: unknown) {
+        ipcRenderer.invoke(channel.setSettings, key, value);
+    },
+    selectGoogleDrive(filePath: string): Promise<boolean> {
+        return ipcRenderer.invoke(channel.selectGoogleDrive, filePath);
+    },
+};
+
+contextBridge.exposeInMainWorld('electron', electronHandler);
+
+export type ElectronHandler = typeof electronHandler;
